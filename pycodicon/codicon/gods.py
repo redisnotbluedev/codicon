d23b84d19858513eedbae352411fbf953e9ea75d
redisnotbluedev <ededmaude@gmail.com>
2025-06-19 21:55:15 +0930

    Add .gods for shorthand

---
+++ pycodicon/codicon/gods.py
@@ -0,0 +1,8 @@
+from .archivus import Archivus
+from .graphos import Graphos
+from .logos import Logos
+from .machina import Machina
+from .segfalux import Segfalux
+from .void import Void
+
+__all__ = ["Archivus", "Graphos", "Logos", "Machina", "Segfalux", "Void"]
